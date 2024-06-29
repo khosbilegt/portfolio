2b9244ab363fbc79be1397682657462712193347
khosbilegt.b <khosbilegt.b@unitel.mn>
2024-06-29 23:00:57 +0800

    meta tags

---
+++ src/app/home/project-carousel/project-carousel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-carousel.component.html',
+  styleUrl: './project-carousel.component.css'
+})
+export class ProjectCarouselComponent {
+
+}
