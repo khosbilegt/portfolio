2225f710d5e1763bfd89a7a367bcb63335354327
khosbilegt.b <khosbilegt.b@unitel.mn>
2024-07-01 00:39:43 +0800

    added project carousel

--- src/app/home/project-carousel/project-carousel.component.ts
+++ src/app/home/project-carousel/project-carousel.component.ts
@@ -2,11 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { AfterViewInit, Component } from '@angular/core';
 import { CarouselModule } from 'primeng/carousel';
 import { ImageModule } from 'primeng/image';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-project-carousel',
   standalone: true,
-  imports: [CarouselModule, ImageModule],
+  imports: [CarouselModule, ImageModule, TagModule],
   templateUrl: './project-carousel.component.html',
   styleUrl: './project-carousel.component.css',
 })
@@ -20,6 +21,7 @@ export class ProjectCarouselComponent implements AfterViewInit {
       .get<any>('http://127.0.0.1:8080/api/blog?type=project')
       .subscribe((response) => {
         this.projects = response.data;
+        console.log(this.projects);
       });
   }
 }
